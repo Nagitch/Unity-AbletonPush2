d6bb91dd1c1af6485db391deb9aa1ae1166592fe
Yu Kusanagi <mujinaproduction@gmail.com>
2019-04-17 01:30:22 +0900

    cosmetic changes

--- Assets/Examples/PseudoPads.cs
+++ Assets/Examples/PseudoPads.cs
@@ -12,15 +12,11 @@ public class PseudoPads : MonoBehaviour
     void Start()
     {
         int index = 0;
-        // Debug.Log(Pads.S1T1.name);
-        // Debug.Log(Pads.All[0].name);
         Pads.All.ForEach(e =>
-        // foreach (Pad e in Pads.All)
         {
             Vector3 pos = new Vector3(transform.position.x + (index % 8) * padSpacing, transform.position.y + (Mathf.FloorToInt(index / 8)) * padSpacing, transform.position.z);
             GameObject go = Instantiate(padBase, pos, Quaternion.identity, this.transform);
             go.transform.Find("PadVisual").gameObject.GetComponent<PseudoPad>().pad = e;
-            Debug.Log(e.name);
             index++;
         });
     }
